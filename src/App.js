c591c5209ba0b52b180302daa14035085f3f1472
Dongeun Kim <pobi555@naver.com>
2024-02-19 21:26:37 +0900

    Feat: 페이지 라우팅 세팅

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from'react-router-dom';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Sign from './pages/Sign';
+import Profile from './pages/Profile';
+import Enroll from './pages/Enroll';
 import Header from './components/Header';
 import Footer from './components/Footer';
-// import About from './pages/About';
 
 function App() {
   return (
@@ -11,7 +14,10 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/about" element={<About />} /> */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/mypage" element={<Profile />} />
+        <Route path="/sign" element={<Sign />} />
+        <Route path="/enroll" element={<Enroll />} />
       </Routes>
       <Footer />
     </Router>
---
+++ src/pages/Enroll.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Enroll = () => {
+  return (
+    <div>등록페이지</div>
+  )
+}
+
+export default Enroll
