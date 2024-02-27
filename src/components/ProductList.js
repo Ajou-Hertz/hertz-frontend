15bca6a943f9224a5871fc305ce4cc7d7d52a3ce
Dongeun Kim <pobi555@naver.com>
2024-02-27 17:12:13 +0900

    Feat: 더미데이터로 메인페이지 개발

---
+++ src/components/ProductList.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+import MarketCard from "./ProductCard.js";
+
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Grid, Container } from "@mui/material";
+
+const theme = createTheme();
+
+function ProductList({ list }) {
+    const marketList = list;
+    console.log(marketList);
+    return (
+        <ThemeProvider theme={theme}>
+            <Container
+                sx={{ py: 8 }}
+                maxWidth="lg"
+                style={{ paddingTop: "10px" }}
+            >
+                <Grid container spacing={8}>
+                    {marketList.map((market) => (
+                        <Grid key={market.id} item xs={12} sm={6} md={3}>
+                            <MarketCard
+                                id={market.id}
+                                img={market.img}
+                                url={market.url}
+                                name={market.name}
+                                location={market.location}
+                                detail={market.detail}
+                                time={market.time}
+                                period={market.period}
+                                like={market.like}
+                                view={market.view}
+                            />
+                        </Grid>
+                    ))}
+                </Grid>
+            </Container>
+        </ThemeProvider>
+    );
+}
+
+export default ProductList;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,112 @@
-import React from 'react'
+import { useState, useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
-const Home = () => {
-  return (
-    <div>메인페이지</div>
-  )
+import axios from "../api/axios";
+
+import MarketList from "../components/ProductList.js";
+import Button from "@mui/material/Button";
+
+import { MarketStateContext } from "../App";
+
+import { useFetch } from "./../hooks/useFetch";
+
+function Home() {
+    const marketList = useContext(MarketStateContext);
+    const navigate = useNavigate();
+
+    const [data, setData] = useState([]); // get으로 가져온 데이터
+    // useEffect(() => {
+    //     axios
+    //         .get(`/markets?sort=LATEST&date=CURRENT&page=0`)
+    //         .then((res) => {
+    //             // setData(res.data.markets);
+    //             // console.log(res.data.markets);
+
+    //             const getData = res.data.markets.map((it) => {
+    //                 const startDate = new Date(it.startDate);
+    //                 const endDate = new Date(it.endDate);
+
+    //                 let period = `${startDate.getFullYear()}.${
+    //                     startDate.getMonth() + 1
+    //                 }.${startDate.getDate()}`;
+    //                 period += ` ~ ${endDate.getFullYear()}.${
+    //                     endDate.getMonth() + 1
+    //                 }.${endDate.getDate()}`;
+
+    //                 return {
+    //                     id: it.marketIdx,
+    //                     img: it.thumbnailImage,
+    //                     url: "https://www.instagram.com/p/CgoeXAELGQl/",
+    //                     name: it.name,
+    //                     location: `${it.address.sido} ${it.address.sigungu}`,
+    //                     detail: it.address.detailAddress,
+    //                     time: `${it.openTime.slice(
+    //                         0,
+    //                         5
+    //                     )} ~ ${it.closeTime.slice(0, 5)}`,
+    //                     period: period,
+    //                     like: it.interestCount,
+    //                     view: it.viewCount,
+    //                 };
+    //             });
+    //             setData(getData);
+    //         })
+    //         .catch(function (error) {
+    //             console.log(error);
+    //         });
+    // }, []);
+
+    // const getClick = () => {
+    //     axios
+    //         .get("/users")
+    //         .then((res) => {
+    //             setData(res.data);
+    //         })
+    //         .catch(function (error) {
+    //             console.log(error);
+    //         });
+    // };
+    // const postClick = () => {
+    //     axios
+    //         .post("users/email", {
+    //             email: "temp000@naver.com",
+    //         })
+    //         // .post("http://52.79.146.185:8080/api/users/email", {
+    //         //     email: "temp000@naver.com",
+    //         // })
+    //         .then((res) => console.log(res.data))
+    //         .catch(function (error) {
+    //             console.log(error);
+    //         });
+    // };
+
+    function handleClick() {
+        navigate("/search");
+    }
+
+    return (
+        <div className="area">
+            <h1
+                className="title"
+                style={{ textAlign: "left", marginLeft: "24px" }}
+            >
+                Hot
+            </h1>
+            <MarketList list={marketList} />
+            <MarketList list={data} />
+            {/* <Button
+                className="button"
+                variant="outlined"
+                onClick={handleClick}
+                sx={{ mt: 2, mb: 2 }} // margin-top과 margin-bottom을 추가합니다.
+            >
+                더보기
+            </Button> */}
+
+            {/* <button onClick={getClick}>get</button>
+            <button onClick={postClick}>post</button> */}
+        </div>
+    );
 }
 
-export default Home
+export default Home;
