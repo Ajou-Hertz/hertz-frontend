e7fe350a69c39cc7467984afc75a2e0f74a12764
Dongeun Kim <pobi555@naver.com>
2024-03-19 01:42:00 +0900

    Feat: 전체 매물 조회 및 일렉기타 조회 API 연동

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -8,8 +8,14 @@ import { Grid, Container } from "@mui/material";
 const theme = createTheme();
 
 function ProductList({ list }) {
-    const marketList = list;
-    console.log(marketList);
+    if (!list || !list.content) {
+        // list가 유효하지 않거나 content가 없을 때
+        return null;
+    }
+
+    const marketList = list.content; // content 배열에서 데이터 추출
+
+    // console.log(marketList);
     return (
         <ThemeProvider theme={theme}>
             <Container
@@ -22,15 +28,14 @@ function ProductList({ list }) {
                         <Grid key={market.id} item xs={12} sm={6} md={3}>
                             <MarketCard
                                 id={market.id}
-                                img={market.img}
-                                url={market.url}
-                                name={market.name}
-                                location={market.location}
-                                detail={market.detail}
-                                time={market.time}
-                                period={market.period}
-                                like={market.like}
-                                view={market.view}
+                                img={
+                                    market.thumbnailImage
+                                        ? market.thumbnailImage.url
+                                        : market.images[0].url
+                                } // 썸네일 이미지 URL
+                                price={market.price}
+                                title={market.title}
+                                location={`${market.tradeAddress.sido} ${market.tradeAddress.sgg} ${market.tradeAddress.emd}`}
                             />
                         </Grid>
                     ))}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import axios from "../api/axios";
 
 import MarketList from "../components/ProductList.js";
 import Button from "@mui/material/Button";
-
+import ProductList from "../components/ProductList.js";
 import { MarketStateContext } from "../App";
 
 import { useFetch } from "./../hooks/useFetch";
@@ -17,74 +17,28 @@ function Home() {
     const navigate = useNavigate();
 
     const [data, setData] = useState([]); // get으로 가져온 데이터
-    // useEffect(() => {
-    //     axios
-    //         .get(`/markets?sort=LATEST&date=CURRENT&page=0`)
-    //         .then((res) => {
-    //             // setData(res.data.markets);
-    //             // console.log(res.data.markets);
 
-    //             const getData = res.data.markets.map((it) => {
-    //                 const startDate = new Date(it.startDate);
-    //                 const endDate = new Date(it.endDate);
+    useEffect(() => {
+        const fetchInstruments = async () => {
+            try {
+                const response = await axios.get("/instruments", {
+                    params: {
+                        page: 0, // 페이지 번호
+                        size: 8, // 페이지당 아이템 수
+                        sort: "CREATED_BY_ASC", // 정렬 방식 (예: 오름차순)
+                    },
+                    headers: {
+                        "Hertz-API-Version": "1", // 헤더에 버전 정보 추가
+                    },
+                });
+                setData(response.data);
+            } catch (error) {
+                console.error("Error fetching instruments:", error);
+            }
+        };
 
-    //                 let period = `${startDate.getFullYear()}.${
-    //                     startDate.getMonth() + 1
-    //                 }.${startDate.getDate()}`;
-    //                 period += ` ~ ${endDate.getFullYear()}.${
-    //                     endDate.getMonth() + 1
-    //                 }.${endDate.getDate()}`;
-
-    //                 return {
-    //                     id: it.marketIdx,
-    //                     img: it.thumbnailImage,
-    //                     url: "https://www.instagram.com/p/CgoeXAELGQl/",
-    //                     name: it.name,
-    //                     location: `${it.address.sido} ${it.address.sigungu}`,
-    //                     detail: it.address.detailAddress,
-    //                     time: `${it.openTime.slice(
-    //                         0,
-    //                         5
-    //                     )} ~ ${it.closeTime.slice(0, 5)}`,
-    //                     period: period,
-    //                     like: it.interestCount,
-    //                     view: it.viewCount,
-    //                 };
-    //             });
-    //             setData(getData);
-    //         })
-    //         .catch(function (error) {
-    //             console.log(error);
-    //         });
-    // }, []);
-
-    // const getClick = () => {
-    //     axios
-    //         .get("/users")
-    //         .then((res) => {
-    //             setData(res.data);
-    //         })
-    //         .catch(function (error) {
-    //             console.log(error);
-    //         });
-    // };
-    // const postClick = () => {
-    //     axios
-    //         .post("users/email", {
-    //             email: "temp000@naver.com",
-    //         })
-    //         // .post("http://52.79.146.185:8080/api/users/email", {
-    //         //     email: "temp000@naver.com",
-    //         // })
-    //         .then((res) => console.log(res.data))
-    //         .catch(function (error) {
-    //             console.log(error);
-    //         });
-    // };
-
-    function handleClick() {
-        navigate("/search");
-    }
+        fetchInstruments();
+    }, []);
 
     return (
         <div className="area">
@@ -104,20 +58,8 @@ function Home() {
                     margin: "0 60px",
                 }}
             >
-                <MarketList list={marketList} />
-                <MarketList list={data} />
+                <ProductList list={data} />
             </div>
-            {/* <Button
-                className="button"
-                variant="outlined"
-                onClick={handleClick}
-                sx={{ mt: 2, mb: 2 }} // margin-top과 margin-bottom을 추가합니다.
-            >
-                더보기
-            </Button> */}
-
-            {/* <button onClick={getClick}>get</button>
-            <button onClick={postClick}>post</button> */}
         </div>
     );
 }
