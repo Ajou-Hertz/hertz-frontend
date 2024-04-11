25d17876329e0a1e65d03eb834b865bc4db763f4
Dongeun Kim <pobi555@naver.com>
2024-04-11 17:55:58 +0900

    Feat: 중고악기 매물페이지 판매상태 필터 기능 구현

--- src/components/Sub/Dropdown/DropdownInAll.js
+++ src/components/Sub/Dropdown/DropdownInAll.js
@@ -1,25 +1,28 @@
-import React, { useState } from 'react';
-import DropdownMenu from './DropdownMenu';
+import React, { useState } from "react";
+import DropdownMenu from "./DropdownMenu";
 
-const DropdownInAll = () => {
+const DropdownInAll = ({ onSelectProgress }) => {
     // options 배열
-    const options = ['판매중', '판매완료', '예약중'];
-  
+    const options = ["판매중", "판매완료", "예약중"];
+
     // 선택된 옵션을 추적하는 상태 및 처리 함수
-    const [selectedOption, setSelectedOption] = useState('');
-  
+    const [selectedOption, setSelectedOption] = useState("");
+
     const handleSelect = (selectedValue) => {
-      setSelectedOption(selectedValue);
-      // 선택한 옵션에 대한 추가 로직 수행 가능
+        setSelectedOption(selectedValue);
+        onSelectProgress && onSelectProgress(selectedValue); // 선택된 값에 대한 처리 함수 호출
     };
-  
+
     return (
-      <div>
-        {/* DropdownMenu 컴포넌트 렌더링 */}
-        <DropdownMenu
-          options={options} onSelect={handleSelect} defaultOptionText="전체" />
-      </div>
+        <div>
+            {/* DropdownMenu 컴포넌트 렌더링 */}
+            <DropdownMenu
+                options={options}
+                onSelect={handleSelect}
+                defaultOptionText="전체"
+            />
+        </div>
     );
-  };
-  
-  export default DropdownInAll;
+};
+
+export default DropdownInAll;
--- src/pages/SubPage/Instrunment/InstrumentList.js
+++ src/pages/SubPage/Instrunment/InstrumentList.js
@@ -1,25 +1,20 @@
-import React, { useState, useEffect, useContext } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 
 import axios from "../../../api/axios.js";
 
 import ProductList from "../../../components/ProductList.js";
-import { MarketStateContext } from "../../../App.js";
 
 import NavBar from "../../../components/Sub/NavBar";
 import DropdownInstrument from "../../../components/Sub/Dropdown/DropdownInstrument";
 import DropdownInAll from "../../../components/Sub/Dropdown/DropdownInAll";
 
 function InstrumentList() {
-    const marketList = useContext(MarketStateContext);
-    const navigate = useNavigate();
-
     const [data, setData] = useState([]); // get으로 가져온 데이터
 
     const [selectedInstrument, setSelectedInstrument] = useState("일렉기타"); // 선택한 악기 종류 상태
+    const [selectedProgress, setSelectedProgress] = useState(""); // 선택한 진행 상태 상태
 
-    const fetchInstrumentData = async (selectedMenuType) => {
-        // 새로운 함수 추가
+    const fetchInstrumentData = async (selectedMenuType, selectedProgress) => {
         try {
             const response = await axios.get(
                 // 선택된 메뉴 유형에 따라 다른 API 호출
@@ -30,8 +25,9 @@ function InstrumentList() {
                 {
                     params: {
                         page: 0, // 페이지 번호
-                        size: 20, // 페이지당 아이템 수
+                        size: 30, // 페이지당 아이템 수
                         sort: "CREATED_BY_DESC", // 정렬 방식 (예: 오름차순)
+                        progress: selectedProgress, // 선택한 진행 상태
                     },
                     headers: {
                         "Hertz-API-Version": "1", // 헤더에 버전 정보 추가
@@ -46,29 +42,57 @@ function InstrumentList() {
 
     useEffect(() => {
         fetchInstrumentData("electric-guitars"); // 초기 데이터 호출
-    }, []); // 초기 렌더링 시 한 번만 호출
+        setSelectedInstrument("electric-guitars");
+    }, []);
 
     const handleInstrumentSelect = (selected) => {
-        fetchInstrumentData(selected);
         switch (selected) {
             case "이펙터":
+                setSelectedInstrument("effectors");
                 fetchInstrumentData("effectors");
                 break;
             case "앰프":
+                setSelectedInstrument("amplifiers");
                 fetchInstrumentData("amplifiers");
                 break;
             case "베이스":
+                setSelectedInstrument("bass-guitars");
                 fetchInstrumentData("bass-guitars");
                 break;
             case "어쿠스틱&클래식":
+                setSelectedInstrument("acoustic-and-classic-guitars");
                 fetchInstrumentData("acoustic-and-classic-guitars");
                 break;
             case "음향장비":
+                setSelectedInstrument("audio-equipments");
                 fetchInstrumentData("audio-equipments");
                 break;
             default:
+                setSelectedInstrument("electric-guitars");
                 fetchInstrumentData("electric-guitars");
         }
+    };
+
+    const handleInstrumentIng = (ing) => {
+        switch (ing) {
+            case "판매중":
+                setSelectedProgress("SELLING");
+                fetchInstrumentData(selectedInstrument, "SELLING");
+                break;
+            case "판매완료":
+                setSelectedProgress("SOLD_OUT");
+                fetchInstrumentData(selectedInstrument, "SOLD_OUT");
+                break;
+            case "예약중":
+                setSelectedProgress("RESERVED");
+                fetchInstrumentData(selectedInstrument, "RESERVED");
+                break;
+            default:
+                setSelectedProgress("");
+                fetchInstrumentData(selectedInstrument, "");
+        }
+        // fetchInstrumentData(selectedInstrument, ing);
+        setSelectedProgress(ing);
     };
 
     return (
@@ -87,7 +111,10 @@ function InstrumentList() {
                     onSelectInstrument={handleInstrumentSelect}
                     style={{ position: "relative", zIndex: 100 }}
                 />
-                <DropdownInAll style={{ position: "relative", zIndex: 100 }} />
+                <DropdownInAll
+                    onSelectProgress={handleInstrumentIng}
+                    style={{ position: "relative", zIndex: 100 }}
+                />
             </div>
             {/* MarketList에 필터된 데이터 전달 */}
             <ProductList list={data} />
