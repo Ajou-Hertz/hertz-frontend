7022445401e734e163bc90dcdabacb326e7a0efa
Jeong Min <alslah1004@gmail.com>
2024-04-26 22:04:10 +0900

    Feat: 합주실 메인 페이지 퍼블리싱

--- src/pages/SubPage/EnsembleRoom/EnsembleRoomList.js
+++ src/pages/SubPage/EnsembleRoom/EnsembleRoomList.js
@@ -13,9 +13,17 @@ import { useFetch } from "../../../hooks/useFetch.js";
 
 import NavBar from "../../../components/Sub/NavBar.js";
 import DropdownInstrument from "../../../components/Sub/Dropdown/DropdownInstrument.js";
-import DropdownInAll from "../../../components/Sub/Dropdown/DropdownInAll.js";
 
 import EnsembleRoomDetail from "./EnsembleRoomDetail.js";
+
+// 공통 드롭다운 스타일
+const dropdownStyle = {
+    backgroundColor: "#D6E0F3",
+    border: "1px solid white",
+    textAlign: "center",
+    width: "150px",
+    height: "30px",
+};
 
 function EnsembleRoomList() {
     const marketList = useContext(MarketStateContext);
@@ -23,13 +31,15 @@ function EnsembleRoomList() {
 
     const [data, setData] = useState([]); // get으로 가져온 데이터
 
-    const [selectedInstrument, setSelectedInstrument] = useState("일렉기타"); // 선택한 악기 종류 상태
+    const [selectedDate, setSelectedDate] = useState("전체"); // 선택한 날짜 종류 상태
+    const [selectedSido, setSelectedSido] = useState("false"); // 선택한 시도 상태
+    const [selectedSgg, setSelectedSgg] = useState("시/군/구"); // 선택한 시군구 상태
 
-    // 선택한 악기 종류에 따라 데이터 필터링
-    // const filteredData = data.filter(
-    //     (item) =>
-    //         selectedInstrument === "all" || item.type === selectedInstrument
-    // );
+
+    // 드롭다운에서 선택된 항목을 처리하는 핸들러
+    const handleDateChange = (e) => {
+        setSelectedDate(e.target.value);
+    };
 
     const gotoDetailPage = () => {
         navigate("/EnsembleRoomDetail");
@@ -65,15 +75,33 @@ function EnsembleRoomList() {
             <NavBar />
             <div
                 style={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                    paddingBottom: "50px",
-                    paddingLeft: "50px",
-                    paddingRight: "50px",
+                    display: "flex", paddingBottom: "50px", paddingLeft: "50px", paddingRight: "50px",
                 }}
             >
-                <DropdownInstrument style={{ position: 'relative', zIndex: 100 }} />
-                <DropdownInAll style={{ position: 'relative', zIndex: 100 }} />
+                {/* 시간 / 일 / 월 선택 드롭다운 */}
+                <select value={selectedDate} onChange={handleDateChange}  style={dropdownStyle}>
+                    <option value="전체">전체</option>
+                    <option value="시간">시간</option>
+                    <option value="일">일</option>
+                    <option value="월">월</option>
+                </select>
+
+                {/* 지역 선택 드롭다운 */}
+                <div style={{ display: 'flex', flexDirection: 'column' }}>
+                    {/* 지역 선택 시 */}
+                    <button onClick={() => setSelectedSido(!selectedSido)}  style={dropdownStyle}>
+                        지역
+                    </button>
+                    {/* 시도 선택 드롭다운 */}
+                    {selectedSido && (
+                        <select style={dropdownStyle}>
+                            <option value="option1">지역 1</option>
+                            <option value="option2">지역 2</option>
+                            <option value="option3">지역 3</option>
+                        </select>
+                    )}
+                </div>
+                {/* <DropdownInstrument style={{ position: 'relative', zIndex: 100 }} /> */}
             </div>
             {/* MarketList에 필터된 데이터 전달 */}
             <ProductList list={data} />
