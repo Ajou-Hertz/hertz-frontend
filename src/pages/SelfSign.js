2accf3e7461698be371344d00e3c4608a577bc7c
Jeong Min <alslah1004@gmail.com>
2024-05-19 18:54:55 +0900

    Feat: 이용약관, 개인정보 동의서

--- src/pages/SelfSign.js
+++ src/pages/SelfSign.js
@@ -18,6 +18,8 @@ import {
     Checkbox,
 } from "@mui/material/";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import AgreementPrivacy from "../components/AgreementPrivacy";
+import TOS from "../components/TOS";
 
 const SelfSign = (props) => {
     const navigate = useNavigate();
@@ -241,7 +243,7 @@ const SelfSign = (props) => {
                                             />
                                         }
                                         label="헤르츠 이용약관 (필수)"
-                                    />
+                                    /> <TOS/>
                                     <FormControlLabel
                                         control={
                                             <Checkbox
@@ -252,7 +254,7 @@ const SelfSign = (props) => {
                                             />
                                         }
                                         label="개인정보 수집 및 이용 (필수)"
-                                    />
+                                    /> <AgreementPrivacy/>
                                     <FormControlLabel
                                         control={
                                             <Checkbox
